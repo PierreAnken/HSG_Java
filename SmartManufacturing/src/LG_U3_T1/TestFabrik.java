9dcd05e4099b74da34b3d76740f51a31baa90cd1
Pierre Anken <pierre.anken@pm.me>
2021-12-10 11:33:51 +0100

    cleanup

--- SmartManufacturing/src/LG_U3_T1/TestFabrik.java
+++ SmartManufacturing/src/LG_U3_T1/TestFabrik.java
@@ -1,6 +1,5 @@
 package LG_U3_T1;
 
-import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
